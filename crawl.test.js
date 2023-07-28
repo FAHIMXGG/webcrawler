3d9f65c4d44719d127d2e175ea1e9154b5484dda
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-07-28 10:51:03 +0600

    test

---
+++ crawl.test.js
@@ -0,0 +1,7 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+test('normalizeURL', () =>{
+    const input = ''
+    const actual = normalizeURL(input)
+})
