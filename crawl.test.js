9682b7027d55cbbd109497ea623404463820d0d3
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-07-28 14:02:51 +0600

    url test

--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,25 @@
 const {normalizeURL} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
-test('normalizeURL', () =>{
-    const input = ''
+test('normalizeURL strip protocol', () =>{
+    const input = 'https://blog.boot.dev/path'
     const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
 })
+test('normalizeURL strip http', () =>{
+    const input = 'http://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+test('normalizeURL strip capitals', () =>{
+    const input = 'http://BLOG.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+
+
