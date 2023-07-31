75b443125370e259148ebe9555dc0c5fe6726f9a
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-07-31 10:57:49 +0600

    crawl single page with err

--- crawl.js
+++ crawl.js
@@ -1,39 +1,64 @@
 const { JSDOM } = require('jsdom')
 
-function getURLsFromHTML(htmlBody, basURL){
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")){
+            console.log(`non html response, content type:${contentType}, on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    }catch(err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+
+}
+
+
+function getURLsFromHTML(htmlBody, basURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
     const linkElements = dom.window.document.querySelectorAll('a')
-    for (const linkElement of linkElements){
-        if (linkElement.href.slice(0, 1) === '/'){
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
             //relative
-            try{
+            try {
                 const urlObj = new URL(`${basURL}${linkElement.href}`)
-            urls.push(urlObj.href)
-            } catch (err){
+                urls.push(urlObj.href)
+            } catch (err) {
                 console.log(`error ${err.message}`)
             }
-            
-        }else{
+
+        } else {
             //ab
-            try{
+            try {
                 const urlObj = new URL(linkElement.href)
-            urls.push(urlObj.href)
-            } catch (err){
+                urls.push(urlObj.href)
+            } catch (err) {
                 console.log(`error abs ${err.message}`)
             }
-            
+
         }
-        
+
     }
     return urls
 }
 
 
-function normalizeURL(urlString){
+function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/'){
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
         return hostPath.slice(0, -1)
     }
     return hostPath
@@ -41,5 +66,6 @@ function normalizeURL(urlString){
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('hello World')
+const { crawlPage } = require('./crawl.js')
+// console.log('hello World')
+
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no websites provided")
+        process.exit(1)
+    }
+    if (process.argv.length < 3) {
+        console.log("too many command")
+        process.exit(1)
+    }
+    // for(const arg of process.argv){
+    //     console.log(arg)
+    // }
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of ${baseURL}`)
+    crawlPage(baseURL)
+}
+
+main()
