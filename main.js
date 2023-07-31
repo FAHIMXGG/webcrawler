95719448e4cfb6a28d3911415fa952077884aa90
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-07-31 11:32:30 +0600

    crawl page

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { crawlPage } = require('./crawl.js')
 // console.log('hello World')
 
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log("no websites provided")
         process.exit(1)
@@ -17,7 +17,11 @@ function main() {
     const baseURL = process.argv[2]
 
     console.log(`starting crawl of ${baseURL}`)
-    crawlPage(baseURL)
+    const pages= await crawlPage(baseURL, baseURL, {})
+
+    for (const page of Object.entries(pages)){
+        console.log(page)
+    }
 }
 
 main()
